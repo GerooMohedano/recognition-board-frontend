e5318d695d04baa1ad25c044a84459938e7bff61
Unknown <romyruiz1993@gmail.com>
2019-04-13 13:02:59 -0300

    Component_Logros

--- src/pages/Perfil/Perfil.js
+++ src/pages/Perfil/Perfil.js
@@ -6,9 +6,9 @@ import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
-  import Radar from 'react-d3-radar';
-  import RadarChart from 'react-svg-radar-chart';
-  import RadioGroup from '@material-ui/core/RadioGroup';
+import Radar from 'react-d3-radar';
+import RadarChart from 'react-svg-radar-chart';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
@@ -17,6 +17,14 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
 require('./Perfil.css');
 
@@ -32,7 +40,15 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
   },
 });
-
+const stylesLogros = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
@@ -50,30 +66,30 @@ class GuttersGrid extends React.Component {
 
     render() {
       const { classes } = this.props;
-   const { spacing } = this.state;
+      const { spacing } = this.state;
 
       const data = [
-   {
-     data: {
-       friendly: 0.7,
-       collaborative: .8,
-       adaptability: 0.9,
-       creativity: 0.67,
-       punctual: 0.8
-     },
-     meta: { color: 'blue' }
-   },
-   {
-     data: {
-       battery: 0.6,
-       design: .85,
-       useful: 0.5,
-       speed: 0.6,
-       weight: 0.7
-     },
-     meta: { color: 'red' }
-   }
- ];
+        {
+           data: {
+             friendly: 0.7,
+             collaborative: .8,
+             adaptability: 0.9,
+             creativity: 0.67,
+             punctual: 0.8
+           },
+           meta: { color: 'blue' }
+         },
+         {
+           data: {
+             battery: 0.6,
+             design: .85,
+             useful: 0.5,
+             speed: 0.6,
+             weight: 0.7
+           },
+           meta: { color: 'red' }
+         }
+      ];
 
  const captions = {
    // columns
@@ -100,7 +116,41 @@ class GuttersGrid extends React.Component {
                 </List>
             </div>
             <div class="box search">
-                <label className="FormField__Label" htmlFor="name">Logros</label>
+                <label className="FormField__Label" htmlFor="name">Mis Logros</label>
+
+                  <div className={classes.root}>
+                   <ExpansionPanel>
+                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                       <Typography className={classes.heading}>Fellowship</Typography>
+                     </ExpansionPanelSummary>
+                     <ExpansionPanelDetails>
+                       <Typography>
+                         Felicidades buen compañero! Obtuviste 10 puntos en Be collaborative
+                       </Typography>
+                     </ExpansionPanelDetails>
+                   </ExpansionPanel>
+                   <ExpansionPanel>
+                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                       <Typography className={classes.heading}>Pro Programer</Typography>
+                     </ExpansionPanelSummary>
+                     <ExpansionPanelDetails>
+                       <Typography>
+                         Felicidades Programador! Obtuviste 10 puntos en Be proffessional
+                       </Typography>
+                     </ExpansionPanelDetails>
+                   </ExpansionPanel>
+                   <ExpansionPanel disabled>
+                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                       <Typography className={classes.heading}>Miss Simpatia</Typography>
+                     </ExpansionPanelSummary>
+                   </ExpansionPanel>
+                   <ExpansionPanel disabled>
+                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                       <Typography className={classes.heading}>Pro Lider</Typography>
+                     </ExpansionPanelSummary>
+                   </ExpansionPanel>
+                 </div>
+
             </div>
             <div class="box messages">
                                     <RadarChart
