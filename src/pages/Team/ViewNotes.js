19a27bf16a212621bc86c5dc0d747f977d585553
Geroo <gero_mohe2@hotmail.com>
2019-05-11 13:03:49 -0300

    Creacion de nuevas notas

--- src/pages/Team/ViewNotes.js
+++ src/pages/Team/ViewNotes.js
@@ -9,49 +9,62 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch'
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
+import Mood from '@material-ui/icons/Mood';
+import MoodBad from '@material-ui/icons/MoodBad';
 
-const notes = [
-  {
-    author: 'Gero',
-    message: "She's an amazing person",
-    isGood: true
-  },
-  {
-    author: 'Poyo',
-    message: "She's done a great job on GTD creation",
-    isGood: true
-  },
-  {
-    author: 'Marcio',
-    message: "E muy inoperante la mina",
-    isGood: false
-  },
-  {
-    author: 'Romy',
-    message: "Insert text in here",
-    isGood: true
-  }
-];
+require('./ViewNotes.css');
+
+const colorForGood = '#3f51b5';
+const colorForBad = '#f50057';
 
 class ViewNotes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      creatingNote: true
+      creatingNote: false,
+      notes: [
+        {
+          author: 'Gero',
+          message: "She's an amazing person",
+          isGood: true
+        },
+        {
+          author: 'Poyo',
+          message: "She's done a great job on GTD creation",
+          isGood: true
+        },
+        {
+          author: 'Marcio',
+          message: "E muy inoperante la mina",
+          isGood: false
+        },
+        {
+          author: 'Romy',
+          message: "Insert text in here",
+          isGood: true
+        }
+      ],
+      newNoteMessage: '',
+      newNotePuntuation: 1,
+      newNoteAuthor: 'Romy'
     };
   }
 
   createCardsWithNotes = () => {
+    const { notes } = this.state;
     const cards = [];
     notes.forEach(note => {
+      const cardColor = note.isGood ? colorForGood : colorForBad;
       cards.push(
-        <Card>
+        <Card style={{ backgroundColor: cardColor }}>
           <CardContent>
-            <Typography gutterBottom component="p">
+            <Typography className="cardParagraph" gutterBottom component="p">
               {`from ${note.author}`}
             </Typography>
-            <Typography component="p">
+            <Typography className="cardParagraph" component="p">
               {note.message}
             </Typography>
           </CardContent>
@@ -61,9 +74,36 @@ class ViewNotes extends React.Component {
     return cards;
   }
 
+  createNewBlankNote = () => {
+    this.setState({ creatingNote: true });
+  }
+
+  handleBlankNoteWriting = () => event => {
+    this.setState({ newNoteMessage: event.target.value });
+  }
+
+  handlePuntuationChange = () => event => {
+    this.setState({ newNotePuntuation: event.target.checked ? -1 : 1 });
+  }
+
+  createNewNoteWithContext = () => {
+    const { newNoteAuthor, newNoteMessage, newNotePuntuation } = this.state;
+    this.setState(prevState => ({
+      notes: [
+        ...prevState.notes,
+        {
+          author: newNoteAuthor,
+          message: newNoteMessage,
+          isGood: newNotePuntuation === 1 ? true : false
+        }
+      ],
+      newNoteMessage: ''
+    }));
+  }
+
   render() {
     const { value, persona, openCreateNote, handleCreateNoteButton } = this.props;
-    const { creatingNote } = this.state;
+    const { creatingNote, newNoteMessage, newNotePuntuation } = this.state;
     return (
       <Dialog
         key={"dialog" + value + persona}
@@ -71,23 +111,51 @@ class ViewNotes extends React.Component {
         onClose={() => handleCreateNoteButton(value, persona, false)}
       >
         <DialogTitle id="form-dialog-title">{value + ' ' + persona}</DialogTitle>
-          <DialogContent>
+          <DialogContent className="cardContainer">
             {this.createCardsWithNotes()}
-            <Card>
-              <CardActionArea>
-                <AddCircleOutline />
-              </CardActionArea>
-            </Card>
-            {(creatingNote) && (
-              <DialogActions>
-                <Button onClick={() => handleCreateNoteButton(value, persona, false)} color="primary">
-                  Cancel
-                </Button>
-                <Button onClick={() => handleCreateNoteButton(value, persona, false)} color="primary">
-                  Create
-                </Button>
-              </DialogActions>
-            )}
+            {(creatingNote)
+            ? (
+              <div>
+                <Card>
+                  <CardContent>
+                    <TextField
+                      id="filled-multiline-flexible"
+                      multiline
+                      rowsMax="10"
+                      value={newNoteMessage}
+                      onChange={this.handleBlankNoteWriting()}
+                      margin="normal"
+                      helperText="Write here your feedback"
+                      variant="outlined"
+                    />
+                    <div>
+                      <Mood color={(newNotePuntuation === 1) ? "primary" : "disabled"} />
+                      <Switch
+                        checked={newNotePuntuation !== 1}
+                        onChange={this.handlePuntuationChange()}
+                        color="default"
+                      />
+                      <MoodBad color={(newNotePuntuation !== 1) ? "secondary" : "disabled"} />
+                    </div>
+                  </CardContent>
+                </Card>
+                <DialogActions>
+                  <Button onClick={() => handleCreateNoteButton(value, persona, false)} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={() => this.createNewNoteWithContext()} color="primary">
+                    Create
+                  </Button>
+                </DialogActions>
+              </div>
+            ) : (
+              <Card>
+                <CardActionArea className="addNewCard" onClick={() => this.createNewBlankNote()}>
+                  <AddCircleOutline />
+                </CardActionArea>
+              </Card>
+            )
+          }
           </DialogContent>
       </Dialog>
     );
